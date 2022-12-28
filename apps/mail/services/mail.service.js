4d13fc4a64c8c9216bbade5d7a2dd5bcfbba1921
Shelly Paxton Eliyahu <shellypax@gmail.com>
2022-12-28 13:27:32 +0200

    mail service debugg

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,5 +1,5 @@
 import { utilService } from '../../../services/util.service.js'
-import {storageService} from '../services/storageService.js'
+// import {storageService} from '../services/storageService.js'
 import { aStorageService } from '../../../services/async-storage.service.js'
 export const mailService = {
     query,
@@ -7,74 +7,86 @@ export const mailService = {
     remove,
     save,
 }
-
+// console.log('storage:',storageService)
+// console.log('astorage:',aStorageService)
 const MAIL_KEY = 'mailDB'
 _createMails()
 
 
-function query () {
+function query() {
     return aStorageService.query(MAIL_KEY)
-        .then(mails=> {
+        .then(mails => {
             return mails
         })
 }
 
 function get(mailId) {
-    return aStorageService.get(MAIL_KEY,mailId)
+    return aStorageService.get(MAIL_KEY, mailId)
 }
 
 function remove(mailId) {
-    return aStorageService.remove(MAIL_KEY,mailId)
+    return aStorageService.remove(MAIL_KEY, mailId)
 }
 
 function save(mail) {
-    if(mail.id) {
-        return aStorageService.put(MAIL_KEY,mail)
+    if (mail.id) {
+        return aStorageService.put(MAIL_KEY, mail)
     } else {
-        return aStorageService.post(MAIL_KEY,mail)
+        return aStorageService.post(MAIL_KEY, mail)
     }
 }
 
 
 
 function _createMails() {
-    let mails = storageService.loadFromStorage(MAIL_KEY)
+    let mails = aStorageService.get(MAIL_KEY)
     if (!mails || !mails.length) {
         mails = [
             {
-                id: utilService.makeId(),
-                subject:'Mister E-Mail',
-                body: Txt,
+                id: utilService._makeId,
+                subject: 'Mister E-Mail',
+                body: 'Hello there, its my first E-mail app!',
                 isRead: false,
                 sendAt: Date.now(),
-                to:'shellypax@gmail.com'
+                to: 'shellypax@gmail.com'
             },
             {
-                id: utilService.makeId(),
-                subject:'Mister E-Mail',
-                body: Txt,
+                id: utilService._makeId,
+                subject: 'Mister E-Mail',
+                body: 'Hello there, its my first E-mail app!',
                 isRead: false,
                 sendAt: Date.now(),
-                to:'shellypax@gmail.com'
+                to: 'shellypax@gmail.com'
             },
             {
-                id: utilService.makeId(),
-                subject:'Mister E-Mail',
-                body: Txt,
+                id: utilService._makeId,
+                subject: 'Mister E-Mail',
+                body: 'Hello there, its my first E-mail app!',
                 isRead: false,
                 sendAt: Date.now(),
-                to:'shellypax@gmail.com'
+                to: 'shellypax@gmail.com'
             },
             {
-                id: utilService.makeId(),
-                subject:'Mister E-Mail',
-                body: Txt,
+                id: utilService._makeId,
+                subject: 'Mister E-Mail',
+                body: 'Hello there, its my first E-mail app!',
                 isRead: false,
                 sendAt: Date.now(),
-                to:'shellypax@gmail.com'
+                to: 'shellypax@gmail.com'
             },
         ]
-    } 
-    
+    }
+    aStorageService.post(MAIL_KEY, mails)
+
 }
 
+// function _createMail() {
+//     const mail = {
+//         id: utilService._makeId(5),
+//         subject: 'Mister E-Mail',
+//         body: 'Hello there, its my first E-mail app!',
+//         isRead: false,
+//         sendAt: Date.now(),
+//         to: 'shellypax@gmail.com'
+//     }
+// }
