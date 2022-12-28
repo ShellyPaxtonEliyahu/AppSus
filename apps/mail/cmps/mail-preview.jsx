3b66f5921e58ddedd73583e7820c8a94c2ecec3e
Shelly Paxton Eliyahu <shellypax@gmail.com>
2022-12-28 15:10:27 +0200

    mails table

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,8 +1,25 @@
-export function MailPreview(){
+const { useState, Fragment } = React
+const { Link } = ReactRouterDOM
 
+export function MailPreview({mail}) {
 
-    function showDetails(mailId) {
-        
-    }
-    
+    const [isExpanded, setIsExpanded] = useState(false)
+
+    return <Fragment>
+        <tr onClick={() => {
+            setIsExpanded(!isExpanded)
+        }}>
+            <td>{mail.subject}</td>
+            <td>{mail.body}</td>
+            {/* <td>
+                <Link to={`/car/${car.id}`}>Details</Link> |
+            </td> */}
+        </tr>
+        <tr hidden={!isExpanded}>
+            <td colSpan="2">
+                <p>{mail.body}</p>
+            </td>
+        </tr>
+    </Fragment>
+
 }
