379520a0bdd9b5010cfa5c4c0a9081f0a9449397
Shelly Paxton Eliyahu <shellypax@gmail.com>
2022-12-28 20:57:16 +0200

    submission 2100

--- views/about.jsx
+++ views/about.jsx
@@ -1,6 +1,24 @@
 
 export function About() {
     return <section className="about">
-        <h1>About Page</h1>
+        <h1>About Us</h1>
+        <div className="about-dana">
+            <img src="../assets/img/dana.jpg" />
+            <h2>Dana Yaniv</h2>
+            <p>live in Ramat-Gan with my spouse Dor, 26YO.
+                Pussion about code ever since high-school, 
+                Having a lot of plants, and a aquarium full of fishes,
+                love to hear from you (972 - 52464399), danayani@gmail.com
+            </p>
+        </div>
+        <div className="about-shelly">
+            <img src="../assets/img/shelly.jpg" />
+            <h2>Shelly Paxton Eliyahu</h2>
+            <p>live in Petah-Tiqwa, 29 YO. married to Naor with 1 YO beutiful child called Lenny.
+                has a bachelor's degree in madicinal chemistry.
+                worked as a branch manager of a asian resturant.
+                love to hear from you (972 - 508372119), shellypax@gmail.com
+            </p>
+        </div>
     </section>
 }
