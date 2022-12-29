11b8255d421cd0ed38ffaa29bd77b39100575b55
Dana Yaniv <danayani@gmail.com>
2022-12-29 19:56:24 +0200

    bvcuhj

--- apps/note/cmps/note-edit.jsx
+++ apps/note/cmps/note-edit.jsx
@@ -2,12 +2,14 @@ const { useState, useEffect } = React
 const { useNavigate, useParams, Link } = ReactRouterDOM
 
 
-export function NoteEdit({noteEdit:note}) {
+export function NoteEdit({ noteEdit: note }) {
 
 
     return <section>
-        NoteEdit
-        {console.log('note', note)}
-        
+        <form>
+            NoteEdit
+            {console.log('note', note)}
+            <button> Update</button>
+        </form>
     </section>
 }
