0196307bc20292b1ace71535cd68130865fc7ca6
Shelly Paxton Eliyahu <shellypax@gmail.com>
2022-12-29 13:34:47 +0200

    filtering

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,13 +1,26 @@
 const { useState, Fragment } = React
-
+import { mailService } from "../services/mail.service.js"
 export function MailPreview({mail}) {
 
     const [isExpanded, setIsExpanded] = useState(false)
+    // const [isMark,setIsMark] = useState(mailService.query())
+
+    function onExpanded(mailId){
+        console.log('mailId',mailId)
+        mailService.get(mailId)
+        .then(() => {
+            mail.isRead = true
+            console.log('mail',mail)
+            setIsExpanded(!isExpanded)
+        })
+        
+        
+    }
+
 
     return <Fragment>
-        <tr onClick={() => {
-            setIsExpanded(!isExpanded)
-        }}>
+        <tr onClick={()=>{
+            onExpanded(mail.id)}}>
             <td>{mail.subject}</td>
             <td>{mail.body}</td>
         </tr>
